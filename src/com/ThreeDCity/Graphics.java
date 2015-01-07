b4ee9197cc0fa733a30ddf979265026bc3825448
jgzuke <jgzuke@gmail.com>
2015-01-07 17:40:57 -0500

    need to fix, project points, and order

--- src/com/ThreeDCity/Graphics.java
+++ src/com/ThreeDCity/Graphics.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.view.View;
 
 /*
@@ -38,30 +39,51 @@ public final class Graphics extends View
 	{
 		ViewRotations view =  getView();
 		ArrayList<int[][]> panels = control.objects.panels;
+		int [] orderToDraw = orderPanels(panels);
 		for(int i = 0; i < panels.size(); i++)
 		{
-			drawPanel(panels.get(i));
+			drawPanel(panels.get(orderToDraw[i]), view, g);
 		}
 		g.drawRect(0, (int)(Math.tan(control.player.zRotation)*view.distanceFromPanel), (int)screenWidth, (int)screenHeight, paint);
+	}
+	/**
+	 * layers panels to be drawn on top of each other
+	 * @return which index will be drawn first, second etc.
+	 */
+	protected int[] orderPanels(ArrayList<int[][]> panels)
+	{
+		int [] orderToDraw = new int[panels.size()];
+		//TODO
+		return orderToDraw;
 	}
 	/**
 	 * draws panel sent to function
 	 * @param panel the panel to draw
 	 */
-	protected void drawPanel(int[][] panel)
+	protected void drawPanel(int[][] rawPanel, ViewRotations view, Canvas g)
 	{
+		int[][] panel = rawPanel.clone();
+		fixPanel(panel);								// fixes panel so it fits on screen
 		
+		Path path = new Path();
+		int[] point = projectOnView(rawPanel[3], view);
+		path.moveTo(point[0], point[1]);				// start at last corner
+		for(int i = 0; i < 4; i++)
+		{
+			point = projectOnView(rawPanel[i], view);	// make lines to each corner
+			path.lineTo(point[0], point[1]);
+		}
+		
+		g.drawPath(path, paint);						// draws polygon
 	}
 	/**
 	 * fixes panel so it can be drawn in bounds
 	 * @param oldPanel	panel sent to function
 	 * @return			panel that can be drawn right
 	 */
-	protected int[][] fixPanel(int[][] oldPanel)
+	protected void fixPanel(int[][] oldPanel)
 	{
-		int[][] panel = oldPanel.clone();
 		//TODO
-		return panel;
 	}
 	/**
 	 * projects a point onto players view and returns x, y screen coordinates
@@ -69,7 +91,7 @@ public final class Graphics extends View
 	 * @param viewPanel	view panel to project onto
 	 * @return			x and y position of point on screen
 	 */
-	protected int [] projectOnView(double [] point, ViewRotations view)
+	protected int [] projectOnView(int [] point, ViewRotations view)
 	{
 		int [] coordinates = new int[2];
 		//TODO
